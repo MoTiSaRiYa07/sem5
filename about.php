4bb66f81694b1ce18c7c333580d410b87fac8b5e
ankushmotisariy <88265954+ankushmotisariy@users.noreply.github.com>
2023-07-19 18:46:33 +0530

    j

--- about.php
+++ about.php
@@ -10,41 +10,19 @@
     <title>Document</title>
 </head>
 <body>
-             <h1>hello</h1>
+            
              <?php include("menu/navbar.php"); ?>
 
-              <h1>MatexNet is India's leading and most reputed private online auction portal operating in the specialised space of buying and selling surplus inventory. Incorporated in 1995, MatexNet provides a comprehensive online marketplace for companies to buy and sell residual and superfluous inventory - from raw material, machinery and industrial by-products to non-performing and stressed assets. Our service offerings include e-procurement of logistics services and transport contracts, tender management, as well as purchase and sale of repossessed vehicles.
+              <p>MatexNet is India's leading and most reputed private online auction portal operating in the specialised space of buying and selling surplus inventory. Incorporated in 1995, MatexNet provides a comprehensive online marketplace for companies to buy and sell residual and superfluous inventory - from raw material, machinery and industrial by-products to non-performing and stressed assets. Our service offerings include e-procurement of logistics services and transport contracts, tender management, as well as purchase and sale of repossessed vehicles.
 
 We offer our services in three distinct verticals:
 
 Industrial
 
 Automobile
+</p>
 
-Transport
-
-First mover advantage
-
-Headquartered in Bangalore, India, MatexNet is India`s first and largest B2B, private, e-commerce portal with over 400 regular sellers and nearly 10000 potential registered buyers. registered on the site. An IT Act, 2000 compliant company, we offer our clients a secure, confidential and dynamic platform to search, identify and bid for a wide range of products in highly focused auctions. Today matexnet.com transacts over Rs 700 crore annually.
-
-For more than a decade we have offered assistance for disposal/procurement of inventory, asset and collaborative requirements of some of the of the country's largest organizations. Our client base includes leading private corporations, public sector undertakings, government bodies, and financial institutions like ABB, Larsen & Tubro, Vedanta Group, Container Corporation of India (CONCOR), HDFC Bank, Aditya Birla Group, National Mineral Development Corporation (NMDC), Bharat Electronics Limited (BEL) and Murugappa Group. (Click here to read some case studies)
-
-Technology
-
-Apart from the unique competitive advantage of being a pioneer in this space, our market leadership is based on the backbone of our company - our technology. The auction software used by MatexNet is a proprietary technology, indigenously developed by our in-house team of experienced IT experts. The software has been designed to be both, intuitive and flexible, allowing it to adapt to market growth, diversity and customer needs. Designed keeping in mind the best practices for online procurement and disposal, it is a robust platform with a properly designed auction process framework, ideal for competitive bidding.
-
-Along with a marked focus on transparency at every stage of the online auction process, MatexNet, with its various auction platforms, offers sellers a strategic tool to maximise revenue, and through a competitive bidding process ensures maximum value for all parties. MatexNet is committed to offering a superior pool of buyers and Sellers. Our strict KYC (Know Your Customer) norms ensure that all participating parties are screened and vetted through a proper identification process, making certain that only, reputed sellers and genuine buyers participate.
-
-Customer driven
-
-We leverage our strength in the precise understanding of business processes of our clients, using the right technology tools to increase internal efficiencies thereby increasing bottom lines. Our in-depth know-how and strong e-auction process framework for forward and reverse auctions empowers us to work closely with several large and medium organizations to manage their inventory and partner relationship opportunities. We utilise our patented technologies to successfully raise efficiency levels, both operational and relationship.
-
-Our service offerings include comprehensive, client-specific solutions for surplus inventory, used plant and machinery and trade/consumer promotion schemes. Our focus on seamless business process execution enables us to provide true partner-type solutions that lead to significantly reduced long term costs and initial implementation expenses - unlike straight-jacketed software products or services vendors whose focus is primarily on the technology and their products.
-
-People speak
-
-MatexNet is managed by a team of committed and experienced leaders, who bring with them speciality knowledge alongside the pre-requisites of being a part of MatexNet - innovation and flexibility. We are a team of over 120 people across the country, with 8 branch offices and have residential representatives in 8 other cities across India. Over the last decade our human resource capital has grown into a quality pool of well-networked specialists with in-depth experience in handling the movement of goods and services in and out of organisations, across industries.</h1>  
-             <!-- partial navbarjs -->
+      <!-- partial navbarjs -->
   <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
   <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>
   <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js'></script>
