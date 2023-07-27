48acc1028908d43419313acdbb19b747e33a416f
MoTiSaRiYa07 <ankushmotisariya@gmail.com>
2023-07-27 14:32:34 +0530

    contus

---
+++ conts.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/55.css">
+    <link rel="stylesheet" type="text/css" href="css/res.css">
+	<title></title>
+</head>
+<body>
+             <section>
+         <div class="contact_us_2">
+  <div class="responsive-container-block big-container">
+    <div class="blueBG">
+    </div>
+    <div class="responsive-container-block container">
+      <form class="form-box">
+        <div class="container-block form-wrapper">
+          <p class="text-blk contactus-head">
+            Get in Touch
+          </p>
+          <p class="text-blk contactus-subhead">
+           PAINTING AUCTION MANGMENT SYSTEAM
+          </p>
+          <div class="responsive-container-block">
+            <div class="responsive-cell-block wk-ipadp-6 wk-tab-12 wk-mobile-12 wk-desk-6" id="i10mt">
+              <p class="text-blk input-title">
+                FIRST NAME
+              </p>
+              <input class="input" id="ijowk" name="FirstName" placeholder="Please enter first name...">
+            </div>
+            <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+              <p class="text-blk input-title">
+                LAST NAME
+              </p>
+              <input class="input" id="indfi" name="Last Name" placeholder="Please enter last name...">
+            </div>
+            <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+              <p class="text-blk input-title">
+                EMAIL
+              </p>
+              <input class="input" id="ipmgh" name="Email" placeholder="Please enter email...">
+            </div>
+            <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+              <p class="text-blk input-title">
+                PHONE NUMBER
+              </p>
+              <input class="input" id="imgis" name="PhoneNumber" placeholder="Please enter phone number...">
+            </div>
+            <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i634i">
+              <p class="text-blk input-title">
+                WHAT DO YOU HAVE IN MIND
+              </p>
+              <textarea class="textinput" id="i5vyy" placeholder="Please enter query..."></textarea>
+            </div>
+          </div>
+          <button class="submit-btn">
+            Submit
+          </button>
+        </div>
+        </form>
+    </div>
+  </div>
+</div>
+</section>
+</body>
+</html>
