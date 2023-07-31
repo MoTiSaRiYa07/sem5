62b770fd4aab49a24225fa8315093843540b314a
MoTiSaRiYa07 <ankushmotisariya@gmail.com>
2023-07-31 10:40:19 +0530

    312-07-2023

--- admin/login.php
+++ admin/login.php
@@ -1,60 +1,100 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <title> Admin Login</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'>
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css'>
-<link rel="stylesheet" href="css/nav.css">
-<link rel="stylesheet" href="css/fo.css">
-</head>
-<body>
-<?php //include('../menu/navbar.php') ?>
-
-<?php
-    require('db.php');
-    session_start();
-    // When form submitted, check and create user session.
-    if (isset($_POST['username'])) {
-        $username = stripslashes($_REQUEST['username']);    // removes backslashes
-        $username = mysqli_real_escape_string($con, $username);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        // Check user is exist in the database
-        $query    = "SELECT * FROM `users` WHERE username='$username'
-                     AND password='" . md5($password) . "'";
-        $result = mysqli_query($con, $query) or die(mysqli_connect_errno());
-        $rows = mysqli_num_rows($result);
-        if ($rows == 1) {
-            $_SESSION['username'] = $username;
-            // Redirect to user dashboard page
-            header("Location: index.php");
-        } else {
-            echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-                  </div>";
-        }
-    } else {
+<html lang="en">
+<?php 
+session_start();
+include('./db_connect.php');
+ob_start();
+if(!isset($_SESSION['system'])){
+	$system = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
+	foreach($system as $k => $v){
+		$_SESSION['system'][$k] = $v;
+	}
+}
+ob_end_flush();
 ?>
-    <form class="form" method="post" name="login">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <link rel="stylesheet" href="../css/style.css">
+
+  <title><?php echo $_SESSION['system']['name'] ?></title>
+ 	
+
+<?php include('./header.php'); ?>
+<?php 
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+
+</head>
+
+<body>
+
+
+  <!--<main id="main" class=" bg-dark">
+  		<div id="login-left">
+  		</div>
+
+  		<div id="login-right">
+  			<div class="card col-md-8">
+  				<div class="card-body">
+  						
+  					<form id="login-form" >
+  						<div class="form-group">
+  							<label for="username" class="control-label">Username</label>
+  							<input type="text" id="username" name="username" class="form-control">
+  						</div>
+  						<div class="form-group">
+  							<label for="password" class="control-label">Password</label>
+  							<input type="password" id="password" name="password" class="form-control">
+  						</div>
+  						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+  					</form>
+  				</div>
+  			</div>
+  		</div>
+   
+
+  </main>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+-->
+
+<form   id="login-form"  class="form" method="post" name="login">
         <h1 class="login-title"> Admin LOGIN</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
         <input type="password" class="login-input" name="password" placeholder="Password"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
-        <p class="link">Don't have an account? <a href="registration.php">Registration Now</a></p>
+       <!-- <p class="link">Don't have an account? <a href="registration.php">Registration Now</a></p>-->
   </form>
 
-<?php
-    }
-?>
-<?php //include ('menu/footer.php'); ?>
 
-<!-- partial -->
-<script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
-<script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>
-<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js'></script>
-<script src='../js/navscript.js'></script>
 </body>
+<script>
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$('#login-form input[type="input"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else{
+					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+					$('#login-form input[type="input"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+</script>	
 </html>
