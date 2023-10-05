cc5f3ea7f3ae85e6df4746b0b4b8a79edd458f2c
MoTiSaRiYa07 <ankushmotisariya@gmail.com>
2023-10-05 18:16:48 +0530

    sjdfn

--- admin/navbar.php
+++ admin/navbar.php
@@ -9,7 +9,7 @@
 		
 		<div class="sidebar-list">
 				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
-				<?php if($_SESSION['login_type'] == 1): ?>
+                    <?php if($_SESSION['login_type'] == 1): ?>
                <a href="index.php?page=categories" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-list"></i></span> Categories</a>
 				<a href="index.php?page=products" class="nav-item nav-products"><span class='icon-field'><i class="fa fa-th-list"></i></span> Products</a>
 			      <a href="index.php?page=bids" class="nav-item nav-bids"><span class='icon-field'><i class="fa fa-money-bill-alt"></i></span> Bids</a>
